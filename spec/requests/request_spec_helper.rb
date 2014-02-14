0a39b5939c06cd3f32a7d6d6d44f6ed46993a553
Jonathan Worek <wojobucco@gmail.com>
2014-02-13 19:59:00 -0500

    Fixed issue on view that doesn't deal with nil @contacts

---
+++ spec/requests/request_spec_helper.rb
@@ -0,0 +1,15 @@
+module RequestSpecHelper
+  def login
+    client = double(GoogleApi, :authorization_uri => 'sessions/authorize',
+      :refresh_token => 'fooey').as_null_object
+
+    data = double('info', name: 'foo', email: 'foo@foo.com', id: '234234')
+    result = double('result')
+    result.stub(:data).and_return(data)
+    client.stub(:get_user_info).and_return(result)
+    client.stub(:access_token).and_return('lkjasdlkfjljasdlkfj_token')
+    GoogleApi.stub(:new).and_return(client)
+
+    get sessions_authorize_path, code: 'foo'
+  end
+end
