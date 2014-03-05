fd75d74149ce83027942f947f26b7f14d0c3779a
Jonathan Worek <wojobucco@gmail.com>
2014-03-05 08:46:51 -0500

    updated contact address

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,11 @@
 require 'bundler/capistrano'
+require 'capistrano/ext/multistage'
+
+set :stages, %w(development production)
+
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
 
 set :application, "calendar-reminders"
 set :repository,  "git@github.com:wojobucco/calendar_reminders.git"
@@ -6,21 +13,8 @@ set :deploy_to, "/var/www/apps/calendar-reminders"
 
 set :scm, :git 
 
-server "ec2-54-85-11-112.compute-1.amazonaws.com", :app, :web, :db, :primary => true
+set :rails_env, 'production'
 
-set :user, "ubuntu"
-ssh_options[:keys] = ["~/.ssh/amazon_aws.pem"]
-ssh_options[:forward_agent] = true
-
-default_run_options[:pty] = true
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
