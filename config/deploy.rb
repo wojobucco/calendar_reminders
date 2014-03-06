80d44b937f879991affa310db7d43d5667c22ad7
Jonathan Worek <wojobucco@gmail.com>
2014-03-05 22:25:59 -0500

    changes for deployment to dev server

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,15 +3,14 @@ require 'capistrano/ext/multistage'
 
 set :stages, %w(development production)
 
-set :default_environment, {
-  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
-}
-
 set :application, "calendar-reminders"
 set :repository,  "git@github.com:wojobucco/calendar_reminders.git"
 set :deploy_to, "/var/www/apps/calendar-reminders"
 
 set :scm, :git 
+
+ssh_options[:forward_agent] = true
+set :user, "ubuntu"
 
 set :rails_env, 'production'
 
