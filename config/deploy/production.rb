ed1d2875ee76dde8051101fa30da5d70cc2ec23e
Jonathan Worek <wojobucco@gmail.com>
2014-03-10 22:30:57 -0400

    A little housekeeping and deployment fix for AWS

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server "ec2-54-85-11-112.compute-1.amazonaws.com", :app, :web, :db, :primary => true
+server "ec2-54-85-91-99.compute-1.amazonaws.com", :app, :web, :db, :primary => true
 
 ssh_options[:keys] = ["~/.ssh/amazon_aws.pem"]
